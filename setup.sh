1f82e7b6b34b4f18a4f0b5ac44b6b5478436f98b
Richard Alexander Haydon <72919977+richarah@users.noreply.github.com>
2022-09-26 17:22:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,4 +37,4 @@ alp qemu-img create -f qcow2 /hda/hda.qcow2 $HDASIZE
 
 # Setup VM
 # TODO: headless setup-alpine
-alp qemu-system-x86_64 -m 512 -netdev user,id=vm0 -device e1000,netdev=vm0 -boot d -cdrom /img/image.iso -hda /hda/hda.qcow2 -nographic
+alp qemu-system-x86_64 -m 4096 -netdev user,id=vm0 -device e1000,netdev=vm0 -boot d -cdrom /img/image.iso -hda /hda/hda.qcow2 -nographic

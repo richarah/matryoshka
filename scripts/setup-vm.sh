25f47931dfaae5e7dd2dee295809dc3728b5276d
richarah <richarah@stud.ntnu.no>
2022-09-26 22:21:21 +0200

    Automation script fixes

--- scripts/setup-vm.sh
+++ scripts/setup-vm.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/expect -f
+set timeout -1
 
 # Setup VM
 spawn qemu-system-x86_64 -m 4096  -netdev user,id=vm0 -device e1000,netdev=vm0 -boot d -cdrom /img/image.iso -hda /tmp/matryoshka.qcow2 -nographic
 
-expect "login: "
+expect "localhost login: "
 send "root\r"
 
-expect "Password: "
-send "\r"
-
-expect "# "
-send "setup-alpine -q && echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories && apk update && apk add docker openrc && poweroff"
+expect "localhost:~# "
+send "setup-alpine -q && echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories && apk update && apk add docker openrc && poweroff\r"
 
